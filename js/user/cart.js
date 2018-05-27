a6b18f639775b21419d841b7e223cb7bac04680a
18230086651 <18230086651@163.com>
2018-05-27 23:15:37 +0800

    购物车

--- js/user/cart.js
+++ js/user/cart.js
@@ -10,8 +10,10 @@ $(function() {
                 info.innerText = 'MUI没有得到你的认可，继续加油'
             }
         });
+            //改变商品数量
+            var number = $('.mui-input-numbox').val();
+            ChangeNumber(number);
     });
-
     var data_list = '';
     getProductListData({
         mark: "product"
@@ -23,11 +25,6 @@ $(function() {
         };
         $('.cart_size').append(data_list);
     });
-
-    //购买数量
-    var a = document.getElementById('numbers');
-    console.log(a);
-
 });
 
 /*获取后台数据 商品列表数据*/
@@ -43,29 +40,21 @@ var getProductListData = function(prams, callback) {
     });
 };
 
+//改变数量个数
+function ChangeNumber(number){
+    var num = 1;
+    $('.mui-btn-numbox-minus').on('tap',function(){
+        if(num>number){
+            num--;
+        }
+        $('.mui-input-numbox').val(num);
+    });
+    $('.mui-btn-numbox-plus').on('tap',function(){
+        num++;
+        $('.mui-input-numbox').val(num);
+    });
+};
 
-
-// <script type="text/javascript" charset="utf-8">
-//              //mui初始化
-//             mui.init({
-//                 swipeBack: true //启用右滑关闭功能
-//             });
-//             var info = document.getElementById("info");
-//             document.getElementById("alertBtn").addEventListener('tap', function() {
-//                 mui.alert('欢迎使用Hello MUI', 'Hello MUI', function() {
-//                     info.innerText = '你刚关闭了警告框';
-//                 });
-//             });
-//             document.getElementById("confirmBtn").addEventListener('tap', function() {
-//                 var btnArray = ['是', '否'];
-//                 mui.confirm('MUI是个好框架，确认？', 'Hello MUI', btnArray, function(e) {
-//                     if (e.index == 0) {
-//                         info.innerText = '你刚确认MUI是个好框架';
-//                     } else {
-//                         info.innerText = 'MUI没有得到你的认可，继续加油'
-//                     }
-//                 })
-//             });
 //             document.getElementById("promptBtn").addEventListener('tap', function(e) {
 //                 e.detail.gesture.preventDefault(); //修复iOS 8.x平台存在的bug，使用plus.nativeUI.prompt会造成输入法闪一下又没了
 //                 var btnArray = ['确定', '取消'];
@@ -80,4 +69,3 @@ var getProductListData = function(prams, callback) {
 //             document.getElementById("toastBtn").addEventListener('tap', function() {
 //                 mui.toast('欢迎体验Hello MUI');
 //             });
-//         </script>
